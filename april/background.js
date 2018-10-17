7d0ca105a3e0c8508f28f6d741b468f6deb6d3f5
April King <april@mozilla.com>
2018-10-17 14:50:43 -0700

    Finalish WebExtension for april

--- april/background.js
+++ april/background.js
@@ -1,22 +1,99 @@
-function listener(details) {
-  let filter = browser.webRequest.filterResponseData(details.requestId);
-  let decoder = new TextDecoder("utf-8");
-  let encoder = new TextEncoder();
+const OBSERVATORY_API = 'https://http-observatory.security.mozilla.org/api/v1';
+const OBSERVATORY_SITE = 'https://observatory.mozilla.org';
 
-  filter.ondata = event => {
-    let str = decoder.decode(event.data, {stream: true});
-    // Just change any instance of Example in the HTTP response
-    // to WebExtension Example.
-    str = str.replace(/Example/g, 'WebExtension Example');
-    filter.write(encoder.encode(str));
-    filter.disconnect();
-  }
+// all the Observatory scores
+const scores = {};
 
-  return {};
+
+// sleep for any number of milliseconds
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
+// this updates the icon to show the letter grade and score, as well as
+// sets the text that you get on hovering to the full Observatory result
+// object
+const updateIcon = async (results, tabId) => {
+  await browser.browserAction.setIcon({
+    path: `icons/${results.grade}.svg`,
+    tabId,
+  });
+
+  await browser.browserAction.setBadgeText({
+    text: results.score.toString(),
+    tabId: tabId,
+  });
+
+  await browser.browserAction.setTitle({
+    title: `The full results are:\n\n${JSON.stringify(results, null, 2)}`,
+    tabId: tabId,
+  });
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-  listener,
-  {urls: ["https://example.com/*"], types: ["main_frame"]},
-  ["blocking"]
+
+// this is the function that goes out to the Observatory and makes the scan
+const scan = async (hostname) => {
+  const formData = new FormData();
+  formData.append('hidden', 'true');
+  formData.append('rescan', 'false');
+
+  const response = await fetch(`${OBSERVATORY_API}/analyze?host=${hostname}`, {
+    body: formData,
+    method: 'POST',
+  });
+
+  return await response.json();
+};
+
+
+// this is the handler that will continue to contact the r
+const scanHandler = async (hostname, tabId) => {
+  let count = 0;
+  let results = { 'state': 'UNKNOWN' };
+  const MAX_ATTEMPTS = 20;
+  const SLEEP_TIME_MILLISECONDS = 1000;
+
+
+  // we'll only scan MAX_ATTEMPTS time, sleeping SLEEP_TIME_MS between each scan
+  while (count < MAX_ATTEMPTS) {
+    count += 1;
+    results = await scan(hostname);
+
+
+    if (['ABORTED', 'FAILED', 'FINISHED'].includes(results.state)) {
+      if (results.state == 'FINISHED') {
+        // update the icon, but only if the scan had successfully finished
+        updateIcon(results, tabId);
+      }
+
+      break;
+    }
+
+    // if the scan hasn't finished, let's sleep for a little bit and try again
+    await sleep(SLEEP_TIME_MILLISECONDS);
+  }
+};
+
+
+// when we get the first response back from the website, initiate the scan
+browser.webRequest.onResponseStarted.addListener(
+  (details) => {
+    results = scanHandler(new URL(details.url).hostname, details.tabId);
+  },
+  {urls: ["http://*/*", "https://*/*"], types: ["main_frame"]}
 );
+
+
+// here is the handler for clicking the browserAction (aka the icon)
+browser.browserAction.onClicked.addListener(async (details) => {
+  // get the current tab hostname
+  console.log('in browseraction', details.url);
+
+  if (details.url !== undefined) {
+    const hostname = new URL(details.url).hostname;
+
+    browser.tabs.create({
+      url: `${OBSERVATORY_SITE}/analyze/${hostname}`,
+    })
+  }
+});
