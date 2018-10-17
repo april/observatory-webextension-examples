82353100cd5021a2bf97d8510ba6dc0f45d9722e
Tristan Weir <tweir@mozilla.com>
2018-10-17 14:23:06 -0700

    Improve tab management

--- tristan/background.js
+++ tristan/background.js
@@ -1,20 +1,36 @@
-// var script = document.createElement('script');
-// script.src = 'https://code.jquery.com/jquery-3.3.1.js';
-// script.type = 'text/javascript';
-// document.getElementsByTagName('head')[0].appendChild(script);
+var currentTab;
+
+// /*
+//  * Switches currentTab and currentBookmark to reflect the currently active tab
+//  */
+async function updateActiveTab(tabs) {
+
+  let gettingActiveTab = await browser.tabs.query({active: true, currentWindow: true});
+  if (gettingActiveTab[0]) {
+      currentTab = gettingActiveTab[0];
+    }
+}
+
+// listen to tab URL changes
+browser.tabs.onUpdated.addListener(updateActiveTab);
+
+// listen to tab switching
+browser.tabs.onActivated.addListener(updateActiveTab);
+
+// listen for window switching
+browser.windows.onFocusChanged.addListener(updateActiveTab);
+
+// update when the extension loads initially
+updateActiveTab();
 
 
 
-async function loadObservatory() {
-
-  let allTabs = await browser.tabs.query({currentWindow: true, active: true});
-  hostname = new URL(allTabs[0].url).hostname;
-  console.log('current tab hostname is ', hostname)
+function loadObservatory() {
+  hostname = new URL(currentTab.url).hostname;
+//  console.log('current tab hostname is ', hostname)
 
   browser.tabs.create({
     url: "https://observatory.mozilla.org/analyze/" + hostname
-
-    // url: "https://http-observatory.security.mozilla.org/api/v1/analyze?host=" + hostname
   });
 }
 
@@ -24,21 +40,25 @@ function updateIcon() {
 }
 
 function updateTooltip(hostname, grade) {
-  browser.browserAction.setTitle(
-    {title: hostname + " - " + grade}
+  if (grade == undefined) {
+    grade = 'Scanning...'
+  }
 
-    );
+  browser.browserAction.setTitle({
+    title: hostname + " - " + grade,
+    tabId: currentTab.id});
 
 }
 
 function runObservatoryScan(requestDetails) {
-  console.log("Scanning: " + requestDetails.url);
   hostname = new URL(requestDetails.url).hostname;
+  console.log("Launching background scan: " + hostname);
+
   observatoryAPIUrl = "https://http-observatory.security.mozilla.org/api/v1/analyze?host=" + hostname;
   
   $.get(observatoryAPIUrl,function( data ) {
 
-    console.log(hostname - data.grade);
+    console.log('Scan Results: ' + hostname + ' - ' + data.grade);
 
     updateIcon(data.grade);
     updateTooltip(hostname, data.grade);
